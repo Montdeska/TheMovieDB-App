1dabed38e1b99dc4f4ae207401e75a04bf7a2218
Abraham <anubbis@gmail.com>
2020-10-14 17:05:53 -0500

    Create MoviesModel.kt

---
+++ app/src/main/java/com/montdeska/tmdb/ui/movies/MoviesModel.kt
@@ -0,0 +1,17 @@
+package com.montdeska.tmdb.ui.movies
+
+class MoviesModel(val headers: HashMap<String, String>) : MoviesContract.Model {
+
+    override suspend fun getPopularData() {
+
+    }
+
+    override suspend fun getUpcomingData() {
+
+    }
+
+    override suspend fun getLatestData() {
+
+    }
+
+}
