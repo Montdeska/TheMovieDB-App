68d0fd4cfd106f4c1cf9913221b25383019384c7
Abraham <anubbis@gmail.com>
2020-10-14 23:55:13 -0500

    Update MoviesModel.kt

--- app/src/main/java/com/montdeska/tmdb/ui/movies/MoviesModel.kt
+++ app/src/main/java/com/montdeska/tmdb/ui/movies/MoviesModel.kt
@@ -1,20 +1,21 @@
 package com.montdeska.tmdb.ui.movies
 
+import com.montdeska.tmdb.ui.data.models.Movie
 import com.montdeska.tmdb.ui.data.models.Movies
 import com.montdeska.tmdb.ui.data.remote.Api
 
-class MoviesModel(val headers: HashMap<String, String>) : MoviesContract.Model {
+class MoviesModel(private val headers: HashMap<String, String>) : MoviesContract.Model {
 
     override suspend fun getPopularData(): Movies {
         return Api.retrofitService.getPopular(headers)
     }
 
-    override suspend fun getUpcomingData() {
-
+    override suspend fun getUpcomingData(): Movies {
+        return Api.retrofitService.getUpcoming(headers)
     }
 
-    override suspend fun getLatestData() {
-
+    override suspend fun getLatestData(): Movie {
+        return Api.retrofitService.getLatest(headers)
     }
 
 }
